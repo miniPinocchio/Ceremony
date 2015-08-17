d58c646cd6135f6b0a8289607557042933e715bc
Blil <715434384@qq.com>
2015-08-17 11:12:09 +0800

    修改app主色调

--- app/src/main/java/com/liuhui/ceremony/app/ui/fragment/PersonalFragment.java
+++ app/src/main/java/com/liuhui/ceremony/app/ui/fragment/PersonalFragment.java
@@ -3,29 +3,29 @@ package com.liuhui.ceremony.app.ui.fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.liuhui.ceremony.app.R;
 import com.liuhui.ceremony.app.base.BaseFragment;
 
-import butterknife.InjectView;
+import butterknife.ButterKnife;
 
 /**
  * "我的"选项卡
  * Created by anany on 15/8/14.
  */
-public class PersonalFragment extends BaseFragment{
+public class PersonalFragment extends BaseFragment {
 
-    @InjectView(R.id.back)
-    ImageView back;
+	@Override
+	public View initView(LayoutInflater inflater, ViewGroup container) {
+		View view = inflater.inflate(R.layout.fragment_personal, container, false);
+		ButterKnife.findById(view, R.id.back).setVisibility(View.GONE);
+		((TextView)ButterKnife.findById(view, R.id.actionBarTitle)).setText(R.string.me);
+		return view;
+	}
 
-    @Override
-    public View initView(LayoutInflater inflater, ViewGroup container) {
-        return inflater.inflate(R.layout.fragment_personal, container, false);
-    }
+	@Override
+	public void initData() {
 
-    @Override
-    public void initData() {
-
-    }
+	}
 }
