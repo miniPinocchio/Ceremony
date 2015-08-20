4eea453b9a749b9eab23ec2404612db6dc1b41e8
Blil <715434384@qq.com>
2015-08-20 20:49:46 +0800

    更新了依赖库版本

--- app/src/main/java/com/liuhui/ceremony/app/ui/fragment/PersonalFragment.java
+++ app/src/main/java/com/liuhui/ceremony/app/ui/fragment/PersonalFragment.java
@@ -1,5 +1,6 @@
 package com.liuhui.ceremony.app.ui.fragment;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,9 +8,11 @@ import android.widget.TextView;
 
 import com.liuhui.ceremony.app.R;
 import com.liuhui.ceremony.app.base.BaseHomeFragment;
+import com.liuhui.ceremony.app.ui.activity.SettingActivity;
 import com.liuhui.ceremony.app.util.LogUtil;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * "我的"选项卡
@@ -31,5 +34,14 @@ public class PersonalFragment extends BaseHomeFragment {
 	@Override
 	public void initData() {
 		LogUtil.e("PersonalFragment initData ---");
+	}
+
+	@OnClick({R.id.setting})
+	void setClickEvent(View view) {
+		switch(view.getId()) {
+			case R.id.setting:
+				startActivity(new Intent(mActivity, SettingActivity.class));
+				break;
+		}
 	}
 }
